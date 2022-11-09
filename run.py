7f184d99a01b705557006ed22fbf04adbc2b20c6
khamdihi-XV <116868920+khamdihi-XV@users.noreply.github.com>
2022-11-09 14:56:35 +0700

    Update run.py

--- run.py
+++ run.py
@@ -4,5 +4,9 @@ from os import system
 from art.main import folder
 system('clear')
 
-if __name__ == '__main__':
-   folder()
+
+if __name__ == "__main__":
+        try:
+                __import__("art").folder()
+        except Exception as e:
+                exit(str(e))
